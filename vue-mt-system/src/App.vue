2fe5632470449f9c782c1f85759084e769f1a1a6
HANXU2018 <1076998404@qq.com>
2021-06-18 13:58:27 +0800

    Sass 语法

--- vue-mt-system/src/App.vue
+++ vue-mt-system/src/App.vue
@@ -1,15 +1,25 @@
 <template>
     <div id="app">
-
+        <p class="app-text">{{data1}}</p>
     </div>
 </template>
 
 <script>
     export default {
         name: 'App',
+        data() {
+            return{
+                data1 : 'P标签'
+            }
+        }
     }
 </script>
 
 <style lang="scss">
-
+    @import "./style/style";
+    $mainColor:blue;
+    .text{
+        color: $mainColor;
+        padding-left: (300px+7px)*2/5;
+    }
 </style>
