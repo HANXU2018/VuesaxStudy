c66b8e532325c25f8884a079dd8c126058a7b709
HANXU2018 <1076998404@qq.com>
2021-06-18 09:38:59 +0800

    父组件传值

--- vue-mt-system/src/App.vue
+++ vue-mt-system/src/App.vue
@@ -1,18 +1,26 @@
 <template>
     <div id="app">
         这里是父组件
+        <HelloWorld :name="name"></HelloWorld>
     </div>
 </template>
 
 <script>
+    import HelloWorld from './components/HelloWorld.vue';
+
     export default {
         name: 'App',
         data() {
-            return {};
-        }
+            return {
+                name: '小明',
+            };
+        },
+        components: {
+            HelloWorld,
+        },
     }
 </script>
 
 <style lang="scss">
-   
+
 </style>
